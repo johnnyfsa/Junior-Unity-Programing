604efa7f9497d99784d4d0d7b3c38a8034fc8a91
johnnyfsa <joaofeirense@gmail.com>
2023-07-25 23:46:06 -0300

    implemented data persistance

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -18,5 +19,36 @@ public class MainManager : MonoBehaviour
         }
        instance = this;
        DontDestroyOnLoad(gameObject);
+       LoadColor();
     }
+
+    public void SaveColor() 
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+
+    public void LoadColor() 
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path)) 
+        { 
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColor = data.teamColor;
+        }
+    }
+
+    [System.Serializable]
+    class SaveData 
+    {
+        public Color teamColor;
+    }
+
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
@@ -23,6 +24,7 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        ColorPicker.SelectColor(MainManager.instance.teamColor);
     }
 
     public void StartNew() 
@@ -30,8 +32,21 @@ public class MenuUIHandler : MonoBehaviour
         SceneManager.LoadScene("Main");
     }
 
+    public void SaveColorClicked() 
+    {
+        MainManager.instance.SaveColor();
+    }
+
+    public void LoadColorClicked() 
+    {
+        MainManager.instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.instance.teamColor);
+    }
+
+
     public void Exit() 
     {
+        MainManager.instance.SaveColor();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
